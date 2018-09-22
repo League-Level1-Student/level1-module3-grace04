ac43710374e07c75b13ed17a849a48473d892f54
blackbeltcam <league@imac.attlocal.net>
2018-09-21 18:27:51 -0700

    Chuckle Clicker

---
+++ src/ChuckleClicker.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class ChuckleClicker {
+	public static void main(String[] args) {
+		ChuckleClicker cc = new ChuckleClicker();
+		cc.makeButtons();
+	}
+	public void makeButtons() {
+		JOptionPane.showMessageDialog(null, "Make Buttons");
+	}
+}
